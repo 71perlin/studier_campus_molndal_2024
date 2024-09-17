ed71f06b56914d676adb8ce56ee2c735f418b6e3
Per Lindberg <71perlin@gafe.molndal.se>
2024-09-17 09:43:15 +0200

    Ny klass

---
+++ Others/Classes/1_OOP_Basics_/Lecture_2/FirstSegment/src/Exercises.java
@@ -0,0 +1,7 @@
+public class Exercises {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+
+    }
+}
---
+++ Others/Classes/1_OOP_Basics_/Lecture_2/FirstSegment/src/PartOne.java
@@ -0,0 +1,56 @@
+public class PartOne {
+    public static void main(String[] args) {
+        System.out.println("Variables and datatypes");
+        System.out.println("Hello World");
+
+        String hello = "Hello World 2";
+        System.out.println(hello);
+
+        Integer age = 19;
+        System.out.println(age);
+
+        double pi = 3.141593;
+        System.out.println(pi);
+
+        char grader = 'G';
+        System.out.println(grader);
+
+        Boolean IsStudent = true;
+        System.out.println(IsStudent);
+
+        System.out.println("------------------------------");
+        System.out.println("Parameters and expressions");
+
+        System.out.println(4+5);
+        System.out.println(4-5);
+        System.out.println(4*5);
+        System.out.println(4/5);
+        System.out.println(4%5);
+        Integer sum = (4 + 5);
+        System.out.println(sum);
+
+        System.out.println("------------------------------");
+
+        System.out.println("Comparative operators");
+
+
+        System.out.println("--------------------------------");
+        System.out.println("Logical operands");
+        // '&&' And
+        // '||' Or
+        // '!' Not
+
+        System.out.println("--------------------------------");
+        System.out.println("Expressions");
+
+
+        System.out.println("--------------------------------");
+        System.out.println("Priority");
+
+        //boolean result = 10 + 5 * 2 // 20
+        boolean result = (10 + 5) * 2 // 30
+        System.out.println(result);
+
+
+    }
+}
